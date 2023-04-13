41bcd37882978df8ed11caa7656b92b2c8f9133c
nicchunglow <iamnicholaschung@gmail.com>
2023-04-14 01:14:23 +0800

    Add stuff

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
 const MeowConfig = {
   General: "Meow",
-  Punctuation: {
-    FullStop: ".",
-  },
 };
+
+const PunctuationArr = ["!", ".", "?", ","];
 
 const ConvertEnglishToMeow = (input) => {
   const inputSplit = input.split(" ");
   let finalSentence = "";
   if (inputSplit.length > 1) {
     const wordArr = [];
-    inputSplit.forEach((_, index) => {
-      const word = index > 0 ? MeowConfig.General.toLowerCase() : MeowConfig.General;
-      wordArr.push(word);
+    inputSplit.forEach((word, index) => {
+      const processedWord = index > 0 ? MeowConfig.General.toLowerCase() : MeowConfig.General;
+      const lastSymbol = word[word.length - 1];
+      wordArr.push(PunctuationArr.includes(lastSymbol) ? processedWord + lastSymbol : processedWord);
     });
     finalSentence = `${wordArr.join(" ")}`;
   }
   finalSentence = finalSentence ? finalSentence : MeowConfig.General;
-  return finalSentence + MeowConfig.Punctuation.FullStop;
+
+  const finalLastWord = finalSentence[finalSentence.length - 1];
+  const finalLastSymbol = finalLastWord[finalLastWord.length - 1];
+
+  return PunctuationArr.includes(finalLastSymbol) ? finalSentence : finalSentence + ".";
 };
 
 module.exports = ConvertEnglishToMeow;
--- main.test.js
+++ main.test.js
@@ -11,9 +11,14 @@ describe("ConvertEnglishToMeow", () => {
     const expected = "Meow meow.";
     expect(result).toBe(expected);
   });
-  // test("if text has !, have it in the sentence.", () => {
-  //   const result = ConvertEnglishToMeow("One Two!");
-  //   const expected = "Meow meow.";
-  //   expect(result).toBe(expected);
-  // });
+  test("should return text is ','", () => {
+    const result = ConvertEnglishToMeow("One, Two three");
+    const expected = "Meow, meow meow.";
+    expect(result).toBe(expected);
+  });
+  test("if text has !, have it in the sentence.", () => {
+    const result = ConvertEnglishToMeow("One Two!");
+    const expected = "Meow meow!";
+    expect(result).toBe(expected);
+  });
 });
